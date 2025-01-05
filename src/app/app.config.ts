4427930cafdcebb1b369843c8b187e459681b49c
JudithFandino <Judithfnd20@gmail.com>
2025-01-05 21:29:30 +0100

    Firebase installation and configuration - Added .env - Added env.d.ts for typed environment variables - Project name change from 'contact_app' to 'contact-app'

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,26 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+
+// import {API_KEY } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: import.meta.env.NG_APP_PROJECT_ID,
+        appId: import.meta.env.NG_APP_APP_ID,
+        storageBucket: import.meta.env.NG_APP_STORAGE_BUCKET,
+        apiKey: import.meta.env.NG_APP_API_KEY,
+        authDomain: import.meta.env.NG_APP_AUTH_DOMAIN,
+        messagingSenderId: import.meta.env.NG_APP_MESSAGING_SENDER_ID,
+      })
+    ),
+    provideFirestore(() => getFirestore()),
+  ],
 };
