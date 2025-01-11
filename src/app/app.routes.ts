17a43388a016305a4233ac756cbfbe1297901d00
JudithFandino <Judithfnd20@gmail.com>
2025-01-11 12:31:22 +0100

    Docs: Add comments in app.routes.ts and contacts.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,14 @@
-import { Routes } from '@angular/router';
+/*
+Angular application main routing configuration.
+Define redirects and lazy loading of modules.
+Redirect root to contact module and manage missing routes.
+*/
 
+import { Routes } from '@angular/router'; // Imports the Routes interface used to define the routes of the application.
+
+// Defined routes array thaat contains all the main routes of the application.
 export const routes: Routes = [
-  { path: '', redirectTo: '/contacts', pathMatch: 'full' },
-  { path: 'contacts', loadChildren: ()=> import('./features/contacts/contacts.routes') },
+  { path: '', redirectTo: '/contacts', pathMatch: 'full' }, //path: When the URL is empty (the root of the site). redirectTo: '/contacts': Redirects the user to the /contacts path. pathMatch: 'full': Only redirects if the URL is completely empty (/).
+  { path: 'contacts', loadChildren: ()=> import('./features/contacts/contacts.routes') }, // path: 'contacts': Activated when the user visits /contacts. loadChildren: Dynamic import (Lazy Loading) of the contacts.routes.ts file. This improves performance by loading modules only when needed.
   { path: '**', redirectTo: '/contacts'} // Redirect any path not found to /contacts
 ];
--- src/app/features/contacts/contacts.routes.ts
+++ src/app/features/contacts/contacts.routes.ts
@@ -1,11 +1,20 @@
-import { Routes } from '@angular/router';
+/*
+Configuration of specific paths for the contacts module.
+Dynamically loads the contact list component.
+Used when navigating to '/contacts'.
+*/
 
+import { Routes } from '@angular/router'; // Import Routes to define the specific routes of the contact module.
+
+// Defines an array of routes for the contact module only.
 const contactsRoute: Routes = [
   {
+    // loadComponent: Dynamically imports the ListComponent component. This is useful for applications with large modules, as it improves performance by loading only what is necessary.
     path: '',
     loadComponent: () => import('./list/list.component').then(m => m.ListComponent),
 
   }
 ];
 
+// Exports the list of routes to be used when importing into the main app.routes.ts file.
 export default contactsRoute;
