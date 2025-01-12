4894201b07a0ba58506e11445fbc1bcbce6c5b88
JudithFandino <Judithfnd20@gmail.com>
2025-01-12 21:22:45 +0100

    Add toolbar structure and style

--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -1,13 +1,32 @@
 import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+
+const MATERIAL_MODULES = [MatToolbarModule, MatIconModule, MatButtonModule]; // creates a constant to pass the modules to import and not to have to add the whole list directly in import
 
 @Component({
   selector: 'app-toolbar',
   standalone: true,
-  imports: [],
+  imports: [MATERIAL_MODULES],
   template: `
-    <p>
-      toolbar works!
-    </p>
+    <mat-toolbar color="primary">
+      <a mat-button routerLink="/">
+        <mat-icon>home</mat-icon>
+        <span>Home</span>
+      </a>
+
+      <a mat-button routerLink="/contacts">
+        <mat-icon>list_alt</mat-icon>
+        <span>Contacts</span>
+      </a>
+
+      <span class="spacer"></span>
+      <a mat-button>
+        <mat-icon>add_box</mat-icon>
+        <span>New contact</span>
+      </a>
+    </mat-toolbar>
   `,
   styles: ``
 })
