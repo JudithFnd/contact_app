4cad5de18afb3c6b630a25c2370af457be20d827
JudithFandino <Judithfnd20@gmail.com>
2025-01-19 17:45:05 +0100

    Add logic to filter component

--- src/app/components/grid/grid.component.ts
+++ src/app/components/grid/grid.component.ts
@@ -1,14 +1,15 @@
-import { Component, input, OnInit, viewChild } from '@angular/core';
+import { Component, effect, input, OnInit, signal, viewChild } from '@angular/core';
 import { MatFormField, MatLabel } from '@angular/material/form-field';
 import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator';
 import { MatSort, MatSortModule } from '@angular/material/sort';
 import {MatTableDataSource, MatTableModule} from '@angular/material/table';
 import {MatInput} from '@angular/material/input';
+import { FilterComponent } from './filter/filter.component';
 
 @Component({
   selector: 'app-grid',
   standalone: true,
-  imports: [MatPaginator, MatTableModule, MatSortModule, MatLabel, MatFormField, MatInput],
+  imports: [MatPaginator, MatTableModule, MatSortModule, FilterComponent],
   templateUrl: './grid.component.html',
   styleUrl: './grid.component.scss'
 })
@@ -20,9 +21,17 @@ export class GridComponent<T> implements OnInit { // <T>: It is a generic that a
   data = input.required<T[]>();
 
   dataSource = new MatTableDataSource<T>();
+  valueToFilter = signal('');
   private readonly _sort = viewChild.required<MatSort>(MatSort);
   private readonly _paginator = viewChild.required<MatPaginator>(MatPaginator);
 
+  constructor() {
+    effect(()=> {
+      if(this.valueToFilter()) {
+        this.dataSource.filter = this.valueToFilter();
+      }
+    }, {allowSignalWrites: true} )
+  }
 
   ngOnInit(): void {
     this.dataSource.data = this.data();
