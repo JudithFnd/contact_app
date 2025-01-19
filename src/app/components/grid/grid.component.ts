195eb9239b3c42ed7cd80aa8b040fb44a6df5249
JudithFandino <Judithfnd20@gmail.com>
2025-01-19 17:09:39 +0100

    Add test filter

--- src/app/components/grid/grid.component.ts
+++ src/app/components/grid/grid.component.ts
@@ -1,12 +1,14 @@
 import { Component, input, OnInit, viewChild } from '@angular/core';
+import { MatFormField, MatLabel } from '@angular/material/form-field';
 import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator';
 import { MatSort, MatSortModule } from '@angular/material/sort';
 import {MatTableDataSource, MatTableModule} from '@angular/material/table';
+import {MatInput} from '@angular/material/input';
 
 @Component({
   selector: 'app-grid',
   standalone: true,
-  imports: [MatPaginator, MatTableModule, MatSortModule],
+  imports: [MatPaginator, MatTableModule, MatSortModule, MatLabel, MatFormField, MatInput],
   templateUrl: './grid.component.html',
   styleUrl: './grid.component.scss'
 })
@@ -26,6 +28,11 @@ export class GridComponent<T> implements OnInit { // <T>: It is a generic that a
     this.dataSource.data = this.data();
     this.dataSource.sort = this._sort();
     this.dataSource.paginator = this._paginator();
+  }
+
+  applyFilter(event:Event): void {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLocaleLowerCase();
   }
 
 }
